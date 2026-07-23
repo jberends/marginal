297d43c99ee62d045911b5c105a5ec561959ab5f
Jochem Berends <jochem.berends@ke-works.com>
2026-07-23 17:34:58 +0200

    Wire live markdown parsing/styling into the editor on text and selection change

--- Sources/Marginal/Document/DocumentViewController.swift
+++ Sources/Marginal/Document/DocumentViewController.swift
@@ -40,6 +40,40 @@ final class DocumentViewController: NSViewController {
 
     func loadInitialText(_ text: String) {
         textView.string = text
+        restyle(cursorLocation: nil)
+    }
+
+    func currentCursorIndex() -> String.Index? {
+        let text = textView.string
+        let location = textView.selectedRange().location
+        guard location != NSNotFound, let range = Range(NSRange(location: location, length: 0), in: text) else { return nil }
+        return range.lowerBound
+    }
+
+    private func restyle(cursorLocation: String.Index?) {
+        let text = textView.string
+        let model = MarkdownDocumentModel(
+            inlineStyles: MarkdownParser.parseInlineStyles(in: text),
+            headers: MarkdownParser.parseHeaders(in: text),
+            listItems: MarkdownParser.parseListItems(in: text),
+            links: MarkdownParser.parseLinks(in: text)
+        )
+        let attributed = MarkdownStyler.attributedString(
+            for: text,
+            model: model,
+            baseFont: textView.font ?? NSFont.systemFont(ofSize: 15),
+            cursorLocation: cursorLocation
+        )
+
+        let selectedRange = textView.selectedRange()
+        isApplyingProgrammaticEdit = true
+        textView.textStorage?.beginEditing()
+        attributed.enumerateAttributes(in: NSRange(location: 0, length: attributed.length), options: []) { attrs, range, _ in
+            textView.textStorage?.setAttributes(attrs, range: range)
+        }
+        textView.textStorage?.endEditing()
+        textView.setSelectedRange(selectedRange)
+        isApplyingProgrammaticEdit = false
     }
 }
 
@@ -48,5 +82,11 @@ extension DocumentViewController: NSTextViewDelegate {
         guard !isApplyingProgrammaticEdit else { return }
         document?.text = textView.string
         document?.updateChangeCount(.changeDone)
+        restyle(cursorLocation: currentCursorIndex())
+    }
+
+    func textViewDidChangeSelection(_ notification: Notification) {
+        guard !isApplyingProgrammaticEdit else { return }
+        restyle(cursorLocation: currentCursorIndex())
     }
 }
